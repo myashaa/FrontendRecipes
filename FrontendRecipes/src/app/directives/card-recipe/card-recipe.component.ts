0fd398cb02856ec7aee5706cc6ff1b367dbe9fe4
Masha <mianto017@mail.ru>
2021-06-08 21:16:33 +0300

    Сделала отображение именно избранных рецептов(стоит звездочка).

--- FrontendRecipes/src/app/directives/card-recipe/card-recipe.component.ts
+++ FrontendRecipes/src/app/directives/card-recipe/card-recipe.component.ts
@@ -10,27 +10,39 @@ export class CardRecipeComponent implements OnInit {
 
   @Input() content!: RecipeDto;
 
-  isFavoriteRecipe: boolean = true;
-  isLikedRecipe: boolean = true;
+  isFavoriteRecipe!: boolean;
+  isLikedRecipe!: boolean;
 
   constructor() { }
 
 
   public switchFavoriteRecipe() {
     this.isFavoriteRecipe = !this.isFavoriteRecipe;
+    (this.content.isFavorite)
+      ? this.content.isFavorite = false
+      : this.content.isFavorite = true;
     (this.isFavoriteRecipe)
-      ? this.content.favorites--
-      : this.content.favorites++
+      ? this.content.favorites++
+      : this.content.favorites--;
   }
 
   public switchLikedRecipe() {
     this.isLikedRecipe = !this.isLikedRecipe;
+    (this.content.isLike)
+      ? this.content.isLike = false
+      : this.content.isLike = true;
     (this.isLikedRecipe)
-      ? this.content.likes--
-      : this.content.likes++
+      ? this.content.likes++
+      : this.content.likes--;
   }
 
   ngOnInit(): void {
+    (this.content.isFavorite) 
+      ? this.isFavoriteRecipe = true
+      : this.isFavoriteRecipe = false;
+    (this.content.isLike)
+      ? this.isLikedRecipe = true
+      : this.isLikedRecipe = false;
   }
 
 }
